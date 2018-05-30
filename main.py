c3dff1c0e38b37f564be4548cd7c8fca36521297
ccsalway <xian@Christians-MacBook-Pro.local>
2018-05-30 23:50:29 +0100

    updates

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from config import *
 import boto3
-
-import sys
-
-for arg in sys.argv:
-    print arg
-
-session = boto3.Session(aws_access_key_id=PROFILE_NAME, aws_secret_access_key=)
-
 
 if __name__ == '__main__':
     pass
--- reset.py
+++
@@ -1,5 +0,0 @@
-import aws.iam.groups
-
-for group in aws.iam.groups.get_groups():
-    if group != "administrator":
-        aws.iam.groups.delete_group(group)
